e037b8718fadab6d650c89ac455ee6f09e1d6e33
Haitam <Haitam.baqloul@hotmail.com>
2024-04-24 22:34:16 +0200

    add a beginning to loginscreen

--- lib/login.screen.dart
+++ lib/login.screen.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  
+  const LoginScreen({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF131921),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
