bb54bb482bc6ba883248db4c6e75cbd78e8bb2ff
MaximilianDuda <maximiliandudajunior@gmail.com>
2024-04-25 15:27:28 +0200

    Add dispose and isFormFilled bool

--- lib/register.dart
+++ lib/register.dart
@@ -19,3 +19,26 @@ class RegisterPage extends StatefulWidget {
   @override
   _RegisterPageState createState() => _RegisterPageState();
 }
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _phoneController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  bool get isFormFilled =>
+      _nameController.text.isNotEmpty &&
+      _emailController.text.isNotEmpty &&
+      _phoneController.text.isNotEmpty &&
+      _passwordController.text.isNotEmpty;
+}
