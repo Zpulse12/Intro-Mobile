0d247a37aa3624c8eed33321b73614820c125aae
MaximilianDuda <maximiliandudajunior@gmail.com>
2024-05-02 11:43:59 +0200

    Make welcomePage startingscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/welcome.dart';
 import 'login.Screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Firebase Login',
-      home: LoginScreen(),
+      home: WelcomePage(),
     );
   }
 }
