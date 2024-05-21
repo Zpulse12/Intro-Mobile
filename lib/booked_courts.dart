5fa8cbc2b292b18a422f987060d542b81d50336b
MaximilianDuda <maximiliandudajunior@gmail.com>
2024-05-21 19:47:57 +0200

    Fix booked courts screen

--- lib/booked_courts.dart
+++ lib/booked_courts.dart
@@ -1,70 +1,94 @@
-import 'package:flutter/material.dart';
+// booked_courts.dart
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class BookedCourtsScreen extends StatefulWidget {
+  const BookedCourtsScreen({super.key});
+
   @override
   _BookedCourtsScreenState createState() => _BookedCourtsScreenState();
 }
 
 class _BookedCourtsScreenState extends State<BookedCourtsScreen> {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
   Future<List<Map<String, dynamic>>> _getUserBookings() async {
-    User? user = _auth.currentUser;
+    User? user = FirebaseAuth.instance.currentUser;
     if (user == null) {
-      return [];
+      throw 'You need to be logged in to view your bookings.';
     }
-    QuerySnapshot querySnapshot = await _firestore
+
+    QuerySnapshot snapshot = await FirebaseFirestore.instance
         .collection('bookings')
         .where('userId', isEqualTo: user.uid)
+        .where('isMatch', isEqualTo: false)
         .orderBy('date', descending: true)
         .get();
 
-    return querySnapshot.docs.map((doc) {
-      return {
-        'courtName': doc['courtName'],
-        'date': (doc['date'] as Timestamp).toDate(),
-        'startTime': doc['startTime'],
-        'endTime': doc['endTime'],
-      };
+    return snapshot.docs.map((doc) {
+      var data = doc.data() as Map<String, dynamic>;
+      data['bookingId'] = doc.id;
+      return data;
     }).toList();
+  }
+
+  Future<void> _cancelBooking(String bookingId) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('bookings')
+          .doc(bookingId)
+          .delete();
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Booking canceled')),
+      );
+      setState(() {});
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to cancel booking: $e')),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Booked Courts'),
+        title: const Text('My Bookings'),
       ),
       body: FutureBuilder<List<Map<String, dynamic>>>(
         future: _getUserBookings(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return Center(child: Text("Error: ${snapshot.error}"));
+            return Center(child: Text('Error: ${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text("No bookings found."));
+            return const Center(child: Text('No bookings found.'));
+          } else {
+            List<Map<String, dynamic>> bookings = snapshot.data!;
+            return ListView.builder(
+              itemCount: bookings.length,
+              itemBuilder: (context, index) {
+                final booking = bookings[index];
+                return Card(
+                  margin: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: Text(booking['courtName']),
+                    subtitle: Text(
+                      'Date: ${DateFormat('dd/MM/yyyy').format((booking['date'] as Timestamp).toDate())}\n'
+                      'Time: ${booking['startTime']} - ${booking['endTime']}',
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.cancel),
+                      onPressed: () async {
+                        await _cancelBooking(booking['bookingId']);
+                      },
+                    ),
+                  ),
+                );
+              },
+            );
           }
-          List<Map<String, dynamic>> bookings = snapshot.data!;
-          return ListView.builder(
-            itemCount: bookings.length,
-            itemBuilder: (context, index) {
-              var booking = bookings[index];
-              return Card(
-                margin: EdgeInsets.all(8),
-                child: ListTile(
-                  title: Text(booking['courtName']),
-                  subtitle: Text(
-                      'Date: ${DateFormat('dd/MM/yyyy').format(booking['date'])}\n'
-                      'Time: ${booking['startTime']} - ${booking['endTime']}'),
-                ),
-              );
-            },
-          );
         },
       ),
     );
