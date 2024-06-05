33175b0b41ffffe50540812585292082fc63bb1d
MaximilianDuda <maximiliandudajunior@gmail.com>
2024-06-06 01:13:31 +0200

    Delete some hardcoded timeslots

--- lib/map.dart
+++ lib/map.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'location_data.dart';
+
 class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,7 +12,7 @@ class MapScreen extends StatelessWidget {
       ),
       body: FlutterMap(
         options: MapOptions(
-          center: LatLng(51.2194, 4.4025),  // Centreren de map rond antwerpen
+          center: LatLng(51.2194, 4.4025), // Centreren de map rond antwerpen
           zoom: 13.0,
         ),
         children: [
@@ -39,36 +40,34 @@ class MapScreen extends StatelessWidget {
     );
   }
 
-void _showLocationDetails(BuildContext context, Location location) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text(location.name),
-        content: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Image.asset(location.imageUrl, fit: BoxFit.cover),
-              const SizedBox(height: 8),
-              Text(location.address),
-              const SizedBox(height: 8),
-              Text('Price: \$${location.price}'),
-              const SizedBox(height: 8),
-              Text('Available Times: ${location.times.join(', ')}'),
-            ],
+  void _showLocationDetails(BuildContext context, Location location) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(location.name),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Image.asset(location.imageUrl, fit: BoxFit.cover),
+                const SizedBox(height: 8),
+                Text(location.address),
+                const SizedBox(height: 8),
+                Text('Price: \$${location.price}'),
+              ],
+            ),
           ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Close'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
-    },
-  );
-}
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
